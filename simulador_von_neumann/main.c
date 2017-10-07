618f1f87ea3c30f4aedc6a50fdd334413f230819
Lelthar <geraldm1998@hotmail.com>
2017-10-07 14:32:01 -0600

    nuevo commit1

--- simulador_von_neumann/main.c
+++ simulador_von_neumann/main.c
@@ -7,7 +7,7 @@ ArquitecturaCPU nuevo2;
 
 int main(){
 	nuevo2.bus_de_datos.entrada = 5;
-	nuevo1.bus_de_datos.entrada = 2;
+	nuevo1.bus_de_datos.entrada = 3;
 	printf("%d\n", nuevo1.bus_de_datos.entrada);
 	printf("%d\n", nuevo2.bus_de_datos.entrada);
 	return 0;
